e170f7f4717487314c96498e9b9b9901864fd3fb
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-07 04:32:34 -0500

    Updates

---
+++ precache-manifest.b9ea6ee153c3df201f1ee8a8ac29f07b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161955064098053b71f712acb6f34558",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2126edb816d10c89fed",
+    "url": "/portfolio/static/css/main.c2a8841a.chunk.css"
+  },
+  {
+    "revision": "c885873c3b3b29744bb9",
+    "url": "/portfolio/static/js/2.0dcc8734.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0dcc8734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2126edb816d10c89fed",
+    "url": "/portfolio/static/js/main.8b9a1721.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
